cde19bafb32e2e098f21933fc1f6a575ebe56c0d
Kirill Zaborsky <avto.oil@gmail.com>
2020-07-21 18:33:12 +0700

    Initial Commit

---
+++ TrafficLightSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Кирилл Заборский on 21.07.2020.
+//  Copyright © 2020 Кирилл Заборский. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
